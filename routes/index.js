6734145437ca52b683bfdbe29d9889b6963d156d
kaivalpatel001 <46737201+kaivalpatel001@users.noreply.github.com>
2019-02-17 20:03:09 -0500

    final changes

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('home.ejs', { title: 'Home' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('about.ejs', { title: 'About' });
 });
 
 /* GET products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+router.get('/projects', function(req, res, next) { 
+  res.render('projects.ejs', { title: 'Projects' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services.ejs', { title: 'Services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('contact.ejs', { title: 'Contact' });
 });
 
 
